a66b4fde87c37cbfc4b55dc0ce9b990ad26baf6d
Maydel-c <165515798+Maydel-c@users.noreply.github.com>
2024-09-25 17:01:23 +0530

    implementing response curve listening to peak filter controls on gui: making the response curve listen to slider movements and change the curve accordingly

--- SSimpleEQ/Source/PluginEditor.cpp
+++ SSimpleEQ/Source/PluginEditor.cpp
@@ -24,11 +24,24 @@ highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlope
     {
         addAndMakeVisible(comp);
     }
+    
+    const auto& params = audioProcessor.getParameters();
+    for(auto param : params)
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
     setSize (600, 400);
 }
 
 SSimpleEQAudioProcessorEditor::~SSimpleEQAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for(auto param : params)
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -116,7 +129,11 @@ void SSimpleEQAudioProcessorEditor::timerCallback()
     if(parametersChanged.compareAndSetBool(false, true))
     {
         // update the mono chain
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         // signal a repaint
+        repaint();
     }
 }
 
